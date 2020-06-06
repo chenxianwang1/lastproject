c1373e90956f002ccb46bac76e558a116d1220f8
chenxianwang1 <2598341163@qq.com>
2020-06-06 13:09:04 +0800

    Initial commit

---
+++ app/src/main/java/com/swufe/last/MainActivity.java
@@ -0,0 +1,16 @@
+package com.swufe.last;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //期末项目
+    }
+}
